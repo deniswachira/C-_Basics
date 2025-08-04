28bea4114abec30e5c92932b77f2a9095cf8e6a4
Denis Wachira <deniswachira77@gmail.com>
2025-08-04 13:01:09 +0300

    oop-classesandobject

---
+++ 4-OOP-ClassesandObjects/4-OOP-ClassesandObjects/Program.cs
@@ -0,0 +1,29 @@
+﻿
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly BirthDay { get; set; }
+
+    public Person(string fname, string lname, DateOnly birthday)
+    {
+        FirstName = fname;
+        LastName = lname;
+        BirthDay = birthday;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($" {FirstName} {LastName}, Born: {BirthDay}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating an object (instance of Person class)
+        Person p1 = new Person("Denis", "Wachira", new DateOnly(1990, 1, 1));
+        p1.DisplayInfo();
+    }
+}
---
+++ testProject/testProject/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using testProject;
+
+
+
+// Creating an object (instance of Person class)
+Person p1 = new Person("Denis", "Wachira", new DateOnly(1990, 1, 1));
+        p1.DisplayInfo();
+
+
